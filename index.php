4a6beb06b0e0c132ee5019b9af0303f1698da36e
Verdejo98 <j.m.verdejo@hotmail.com>
2025-05-09 02:26:49 -0400

    Detección del recurso solicitado en el url

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 header('Content-Type: application/json');
-
+$puerto1 = null;
+$puerto2 = 3366;
 $socket1 = null;
 $socket2 = "/data/data/com.termux/files/usr/var/run/mysqld.sock";
-$conexion = new mysqli("localhost", "root","", "ciisa_backend_v1_eva2_A", 8080, $socket2);
+$conexion = new mysqli("localhost", "root","", "ciisa_backend_v1_eva2_A", $puerto1, $socket1);
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
@@ -11,18 +12,24 @@ if ($conexion->connect_error) {
     echo "Conexión exitosa";
 }
 
-//Tomamos el metodo y la ruta solicitada en el url
+//Tomamos el metodo y el recurso solicitado en el url
 
-$method = $_SERVER['REQUEST_METHOD'];
+$metodo = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $segmentos = explode('/', trim($uri, '/'));
 
-$recurso = $segmentos[0] ?? null; 
-$id = $segmentos[1] ?? null;  
+$recurso = $segmentos[1] ?? null; 
+$id = $segmentos[2] ?? null;  
 
+echo "\nUri: " . $uri . "\n";
+echo "Recurso: " . $recurso;
+echo "\nID: " . $id;
+
+
+/*
 switch ($recurso) {
-    case 'usuarios':
-        switch ($method) {
+    case 'mantenimiento_info':
+        switch ($metodo) {
             case 'GET':
                 if ($id) {
                     getUsuarioPorId($id);
@@ -54,7 +61,18 @@ switch ($recurso) {
                 echo json_encode(["error" => "Método no permitido"]);
         }
         break;
-
+    case 'categoria_servicio':
+        break;
+    case 'info_contacto':
+        break;
+    case 'imagen':
+        break;
+    case 'historia':
+        break;
+    case 'equipo':
+        break;    
+    case 'pregunta_frecuente':
+        break;
     default:
         http_response_code(404);
         echo json_encode(["error" => "Recurso no encontrado"]);
@@ -90,6 +108,6 @@ function actualizarUsuario($id) {
 function eliminarUsuario($id) {
     http_response_code(204); // Sin contenido
 }
-
+*/
 
 ?>
